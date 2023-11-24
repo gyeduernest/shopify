2eb6086db718846366a9f4aaff932d5227aed184
ernest <gyeduernest@gmail.com>
2023-11-24 22:05:12 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+
+// Your javascript goes here
+function alert() {
+  document.getElementById("alert-container").style.display = "block"
+  
+}
+
+function alert2() {
+  document.getElementById("alert-container").style.display="none"
+}
+
+function onSettings() {
+  document.getElementById("settings-container").style.display="block"
+  
+}
+
+function onSettings2() {
+  document.getElementById("settings-container").style.display="none"
+  
+}
+
+function firstStep() {
+  document.getElementById("step-1-container1").style.display="flex"
+  document.getElementById("step-2-container2").style.display="none"
+  document.getElementById("step-3-container3").style.display="none"
+  document.getElementById("step-4-container4").style.display="none"
+  document.getElementById("step-5-container5").style.display="none"
+}
+
+function firstStepButton(){
+  document.getElementById("step-1-container1").style.display="none"
+  document.getElementById("step-2-container2").style.display="flex"
+  document.getElementById("step-3-container3").style.display="none"
+  document.getElementById("step-4-container4").style.display="none"
+  document.getElementById("step-5-container5").style.display="none"
+
+}
+
+function secondStep() {
+  document.getElementById("step-1-container1").style.display="none"
+  document.getElementById("step-2-container2").style.display="flex"
+  document.getElementById("step-3-container3").style.display="none"
+  document.getElementById("step-4-container4").style.display="none"
+  document.getElementById("step-5-container5").style.display="none"
+}
+
+function secondStepButton(){
+  document.getElementById("step-1-container1").style.display="none"
+  document.getElementById("step-2-container2").style.display="none"
+  document.getElementById("step-3-container3").style.display="flex"
+  document.getElementById("step-4-container4").style.display="none"
+  document.getElementById("step-5-container5").style.display="none"
+
+}
+
+
+
+
+function thirdStep() {
+  document.getElementById("step-1-container1").style.display="none"
+  document.getElementById("step-2-container2").style.display="none"
+  document.getElementById("step-3-container3").style.display="flex"
+  document.getElementById("step-4-container4").style.display="none"
+  document.getElementById("step-5-container5").style.display="none"}
+  
+  function thirdStepButton(){
+    document.getElementById("step-1-container1").style.display="none"
+    document.getElementById("step-2-container2").style.display="none"
+    document.getElementById("step-3-container3").style.display="none"
+    document.getElementById("step-4-container4").style.display="flex"
+    document.getElementById("step-5-container5").style.display="none"
+  
+  }
+
+
+function fourthStep() {
+  document.getElementById("step-1-container1").style.display="none"
+  document.getElementById("step-2-container2").style.display="none"
+  document.getElementById("step-3-container3").style.display="none"
+  document.getElementById("step-4-container4").style.display="flex"
+  document.getElementById("step-5-container5").style.display="none"
+}  
+  
+  function fourthStepButton(){
+    document.getElementById("step-1-container1").style.display="none"
+    document.getElementById("step-2-container2").style.display="none"
+    document.getElementById("step-3-container3").style.display="none"
+    document.getElementById("step-4-container4").style.display="none"
+    document.getElementById("step-5-container5").style.display="flex"
+    
+  }
+  function fifthStep() {
+    document.getElementById("step-1-container1").style.display="none"
+  document.getElementById("step-2-container2").style.display="none"
+  document.getElementById("step-3-container3").style.display="none"
+  document.getElementById("step-4-container4").style.display="none"
+  document.getElementById("step-5-container5").style.display = "flex"
+}
+
+function fifthStepButton(){
+  document.getElementById("step-1-container1").style.display="flex"
+  document.getElementById("step-2-container2").style.display="none"
+  document.getElementById("step-3-container3").style.display="none"
+  document.getElementById("step-4-container4").style.display="none"
+  document.getElementById("step-5-container5").style.display="none"
+
+}
+
